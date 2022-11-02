58eb87b73c307ee20675fbf85064ebe67fb7947a
Aliaksei Rudak <lexarudak@gmail.com>
2022-11-02 18:20:28 +0600

    feat: solve 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,16 @@
 // task start
+
+const path = require('path')
+const fs = require('fs')
+const textAddress = path.join(__dirname, 'text.txt')
+const readStream = fs.createReadStream(textAddress)
+let answer = ''
+
+readStream.on('data', function (chunk) {
+  answer += chunk.toString()
+})
+
+readStream.on('end', function () {
+  console.log(answer);
+})
+
