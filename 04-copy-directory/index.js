488fd25803fd937b7d0654a1df836716d4ef494c
Aliaksei Rudak <lexarudak@gmail.com>
2022-11-06 15:20:11 +0600

    fix: copy function remove old documents before copy now

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,18 +4,30 @@ const folder = path.join(__dirname, 'files')
 const newFolder = path.join(__dirname, 'files-copy')
 
 
-fs.mkdir(newFolder, { recursive: true }, (err) => {
-  if (err) {
-    console.log(err.message);  
-  }
-  fs.readdir(folder, (err, data) => {
+
+function copy(dir, newDir) {
+  fs.readdir(dir, {withFileTypes: true}, (err, data) => {
     data.forEach(value => {
-      fs.copyFile(path.join(folder, value), path.join(newFolder, value), (err) => {
-        if (err) {
-          console.log(err.message);
-        }
-      })
+      if (!value.isFile()) {
+        fs.mkdir(path.join(newDir, value.name), { recursive: true }, (err) => {
+          if (err) {
+            console.log(err.message);  
+          }
+        })
+        copy((path.join(dir, value.name)), (path.join(newDir, value.name)))
+      } else {
+        fs.copyFile(path.join(dir, value.name), path.join(newDir, value.name), (err) => {
+          if (err) {
+            console.log(err.message);
+          }
+        })
+      }
     })
   })
- })
+}
 
+fs.rm(newFolder, { recursive: true, force: true }, () => {
+  fs.mkdir(newFolder, { recursive: true }, () => {
+    copy(folder, newFolder)
+  })
+})
